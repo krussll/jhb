922fdd4b116bae9a98f1b3a61e408377e93f088a
krussll <raw987@hotmail.com>
2025-10-22 12:02:00 +0100

    Enable smooth scrolling on getting started anchors

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,6 +3,7 @@ $(function () {
 
   var $postBody = $('.post-article__body')
   var $drillFeature = $('#drill-pack-feature')
+  var $gettingStarted = $('.getting-started')
 
   if ($postBody.length && $drillFeature.length) {
     var $paragraphs = $postBody.find('p')
@@ -12,5 +13,37 @@ $(function () {
     } else {
       $postBody.append($drillFeature)
     }
+  }
+
+  if ($gettingStarted.length) {
+    $gettingStarted.on('click', 'a[href^="#"]', function (event) {
+      var targetSelector = this.getAttribute('href')
+
+      if (!targetSelector || targetSelector === '#') {
+        return
+      }
+
+      var $target = $(targetSelector)
+
+      if (!$target.length) {
+        return
+      }
+
+      event.preventDefault()
+
+      $('html, body').animate(
+        {
+          scrollTop: $target.offset().top,
+        },
+        500,
+        'swing',
+        function () {
+          $target.attr('tabindex', '-1').focus()
+          $target.one('blur', function () {
+            $(this).removeAttr('tabindex')
+          })
+        }
+      )
+    })
   }
 })
